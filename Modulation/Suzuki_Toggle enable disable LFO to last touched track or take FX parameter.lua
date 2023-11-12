61c5a30cee9966248fcb3ed6d7a2b4d01bef95a1
Suzuki <101462564+Suzuki-Re@users.noreply.github.com>
2023-11-13 06:49:52 +0900

    v1.0

---
+++ Modulation/Suzuki_Toggle enable disable LFO to last touched track or take FX parameter.lua
@@ -0,0 +1,31 @@
+-- @description Toggle enable/disable LFO to last touched track or take FX parameter
+-- @author Suzuki
+-- @license GPL v3
+-- @version 1.0
+-- @changelog Initial Release
+-- @about
+
+local r = reaper
+
+local track = r.GetSelectedTrack2(0, 0, true)
+local retval, trackidx, itemidx, takeidx, fxidx, parm = r.GetTouchedOrFocusedFX(0)
+
+if itemidx ~= -1 then
+    local item = r.GetMediaItem(0, itemidx)
+    local take = r.GetMediaItemTake(item, takeidx)
+    local retval, buf = r.TakeFX_GetNamedConfigParm(take, takeidx, "param."..parm..".lfo.active")
+    if retval and buf == "1" then
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".lfo.active", 0)
+    else
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".lfo.active", 1)
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".mod.visible", 1)
+    end
+else
+    local retval, buf = r.TrackFX_GetNamedConfigParm(track, fxidx, "param."..parm..".lfo.active") -- Active(true, 1), Deactivated(true, 0), UnsetYet(false) 
+    if retval and buf == "1" then -- Toggle
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".lfo.active", 0)
+    else
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".lfo.active", 1)
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".mod.visible", 1)
+    end
+end
---
+++ Modulation/Suzuki_Toggle set midi CC link to last touched track or take FX parameter (user input).lua
@@ -0,0 +1,96 @@
+-- @description Toggle set midi CC link to last touched track or take FX parameter (user input)
+-- @author Suzuki
+-- @license GPL v3
+-- @version 1.0
+-- @changelog Initial Release
+-- @about
+
+local r = reaper
+
+local track = r.GetSelectedTrack2(0, 0, true)
+local retval, trackidx, itemidx, takeidx, fxidx, parm = r.GetTouchedOrFocusedFX(0)
+
+local function GetUserInputCC()
+    local retval, retvals_csv = r.GetUserInputs('Set CC value', 2, 'CC value(CC=0_119/14bit=0_31),14bit (yes=1/no=0)', '0,0') -- retvals_csv returns "input1,input2"
+    local input1val, input2val = retvals_csv:match("([^,]+),([^,]+)")
+    if input2val == nil then
+        retvals = nil -- To make global retvals nil, when users choose cancel or close the window 
+    end
+    if input2val ~= nil then
+        if type(input1val) == "string" then
+            local input1check = tonumber(input1val)
+            local input2check = tonumber(input2val)
+            if input1check and input2check then
+                input1val = input1check
+                input2val = input2check
+            else
+                error('Only enter a number')
+            end 
+        end 
+        local input1val = tonumber(input1val)
+        local input2val = tonumber(input2val)                                                      
+        if input2val < 0 then  
+            input2val = 0
+        elseif input2val > 1 then
+            input2val = 1
+        end
+        if input1val < 0 then  
+            input1val = 0
+        elseif input2val == 0 and input1val > 119 then
+            input1val = 119
+        elseif input2val == 1 and input1val > 31 then
+            input1val = 31
+        end
+        input2val = input2val * 128
+        retvals = input1val + input2val
+    end
+    return retvals
+end
+
+if itemidx ~= -1 then
+    local item = r.GetMediaItem(0, itemidx)
+    local take = r.GetMediaItemTake(item, takeidx)
+    local retval, buf = r.TakeFX_GetNamedConfigParm(take, takeidx, "param."..parm..".plink.active")
+    if retval and buf == "1" then
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".plink.active", 0)
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".plink.effect", 0)
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".plink.param", 0)
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".plink.midi_bus", 0)
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".plink.midi_chan", 0)
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".plink.midi_msg", 0)
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".plink.midi_msg2", 0)
+    else
+        GetUserInputCC()
+        if retvals then
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".plink.active", 1)
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".plink.effect", -100)
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".plink.param", -1)
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".plink.midi_bus", 0)
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".plink.midi_chan", 1)
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".plink.midi_msg", 176)
+        r.TakeFX_SetNamedConfigParm(take, takeidx, "param."..parm..".plink.midi_msg2", retvals)
+        end
+    end
+else
+    local retval, buf = r.TrackFX_GetNamedConfigParm(track, fxidx, "param."..parm..".plink.active")
+    if retval and buf == "1" then
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.active", 0)
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.effect", 0)
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.param", 0)
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.midi_bus", 0)
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.midi_chan", 0)
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.midi_msg", 0)
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.midi_msg2", 0)
+    else
+        GetUserInputCC()
+        if retvals then
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.active", 1)
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.effect", -100)
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.param", -1)
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.midi_bus", 0)
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.midi_chan", 1)
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.midi_msg", 176)
+        r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.midi_msg2", retvals)
+        end
+    end
+end
