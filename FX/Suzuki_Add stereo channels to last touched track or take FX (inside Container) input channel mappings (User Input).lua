f79594f507368509a91b6e4a94bfab2475dd6d58
Suzuki <101462564+Suzuki-Re@users.noreply.github.com>
2023-12-09 19:10:51 +0900

    v1.0

--- FX/Suzuki_Add stereo channels to last touched track or take FX (inside Container) input channel mappings (User Input).lua
+++ FX/Suzuki_Add stereo channels to last touched track or take FX (inside Container) input channel mappings (User Input).lua
@@ -1,4 +1,4 @@
--- @description Add stereo channels to last touched track or take FX (inside Container) input channel mapping (User Input)
+-- @description Add stereo channels to last touched track or take FX (inside Container) input channel mappings (User Input)
 -- @author Suzuki
 -- @license GPL v3
 -- @version 1.0
@@ -43,23 +43,24 @@ local right = left + 1
 
 local function TakeFX_SetPin(a, b, c, d, e, f, g, h)
   r.TakeFX_SetPinMappings(take, fxidx, 0, 0, low32l + a, high32l + e) -- #3 input 0, #4 pin 0 left
-  r.TakeFX_SetPinMappings(take, fxidx, 0, 1, low32r + b, high32r + f) -- #4 pin 1 righ
+  r.TakeFX_SetPinMappings(take, fxidx, 0, 1, low32r + b, high32r + f) -- #4 pin 1 right
   r.TakeFX_SetPinMappings(take, fxidx, 0, 0 + 0x1000000, n_low32l + c, n_high32l + g) -- #3 input 0, #4 pin 0 left and add 0x1000000 to #4 for the second half blocks
   r.TakeFX_SetPinMappings(take, fxidx, 0, 1 + 0x1000000, n_low32r + d, n_high32r + h) -- #4 pin 1 right
 end
 
 local function TrackFX_SetPin(a, b, c, d, e, f, g, h)
   r.TrackFX_SetPinMappings(track, fxidx, 0, 0, low32l + a, high32l + e) -- #3 input 0, #4 pin 0 left
-  r.TrackFX_SetPinMappings(track, fxidx, 0, 1, low32r + b, high32r + f) -- #4 pin 1 righ
+  r.TrackFX_SetPinMappings(track, fxidx, 0, 1, low32r + b, high32r + f) -- #4 pin 1 right
   r.TrackFX_SetPinMappings(track, fxidx, 0, 0 + 0x1000000, n_low32l + c, n_high32l + g) -- #3 input 0, #4 pin 0 left and add 0x1000000 to #4 for the second half blocks
   r.TrackFX_SetPinMappings(track, fxidx, 0, 1 + 0x1000000, n_low32r + d, n_high32r + h) -- #4 pin 1 right
 end
 
+r.Undo_BeginBlock()
 if itemidx ~= -1 then -- take FX
   local isincontainer, parent_container = r.TakeFX_GetNamedConfigParm(take, fxidx, 'parent_container')
   if isincontainer then
     local _, hm_cch = r.TakeFX_GetNamedConfigParm(take, parent_container, 'container_nch')
-    if ch_num > hm_cch then
+    if ch_num > tonumber(hm_cch) then
       if ch_num % 2 == 1 then
         r.TakeFX_SetNamedConfigParm(take, parent_container, 'container_nch', ch_num + 1)
       else
@@ -68,7 +69,7 @@ if itemidx ~= -1 then -- take FX
     end
   else
     local hm_ch = r.GetMediaItemTakeInfo_Value(take, 'I_TAKEFX_NCH')
-    if ch_num > hm_ch then
+    if ch_num > tonumber(hm_ch) then
       if ch_num % 2 == 1 then
         r.SetMediaItemTakeInfo_Value(take, 'I_TAKEFX_NCH', ch_num + 1)
       else
@@ -97,7 +98,7 @@ else --  track FX
   local isincontainer, parent_container = r.TrackFX_GetNamedConfigParm(track, fxidx, 'parent_container')  
     if isincontainer then
       local _, hm_cch = r.TrackFX_GetNamedConfigParm(track, parent_container, 'container_nch')
-      if ch_num > hm_cch then
+      if ch_num > tonumber(hm_cch) then
         if ch_num % 2 == 1 then
           r.TrackFX_SetNamedConfigParm(track, parent_container, 'container_nch', ch_num + 1)
         else
@@ -106,7 +107,7 @@ else --  track FX
       end
     else
       local hm_ch = r.GetMediaTrackInfo_Value(track, 'I_NCHAN')
-      if ch_num > hm_ch then
+      if ch_num > tonumber(hm_ch) then
         if ch_num % 2 == 1 then
           r.SetMediaTrackInfo_Value(track, 'I_NCHAN', ch_num + 1)
         else
@@ -132,3 +133,4 @@ else --  track FX
     end
   end
 end
+r.Undo_EndBlock("Add stereo channels to last touched FX input channel mappings", -1)
