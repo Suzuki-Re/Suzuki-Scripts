7de8877f7546c0ed739b160eaee4581fb7b60a22
Suzuki <101462564+Suzuki-Re@users.noreply.github.com>
2023-12-09 15:43:26 +0900

    v1.1

--- FX/Suzuki_Paste channel pin mappings into last touched track or take FX (inside Container) stereo output channel pin mappings.lua
+++ FX/Suzuki_Paste channel pin mappings into last touched track or take FX (inside Container) stereo output channel pin mappings.lua
@@ -1,8 +1,8 @@
 -- @description Paste channel pin mappings into last touched track or take FX (inside Container) stereo output channel pin mappings
 -- @author Suzuki
 -- @license GPL v3
--- @version 1.0
--- @changelog Initial Release
+-- @version 1.1
+-- @changelog Bug fix for container FX
 -- @about Using 7.07+ API, up to 128 channels
 
 local r = reaper
@@ -19,13 +19,14 @@ local rv, right = r.GetProjExtState(0, 'Suzuki_copy_ch', 'right')
 local low32r, high32r, n_low32r, n_high32r = string.match(right, "(%d+),(%d+),(%d+),(%d+)")
 local low32r, high32r, n_low32r, n_high32r = tonumber(low32r), tonumber(high32r), tonumber(n_low32r), tonumber(n_high32r)
 
+r.Undo_BeginBlock()
 if itemidx ~= -1 then -- take FX
     local item = r.GetMediaItem(0, itemidx)
     local take = r.GetMediaItemTake(item, takeidx)
     local isincontainer, parent_container = r.TakeFX_GetNamedConfigParm(take, fxidx, 'parent_container')
     if isincontainer then -- for container FX
         local _, hm_ch = r.TakeFX_GetNamedConfigParm(take, parent_container, 'container_nch')
-        if ch_num > hm_ch then
+        if ch_num > tonumber(hm_ch) then
             if ch_num % 2 == 1 then
                 r.TakeFX_SetNamedConfigParm(take, parent_container, 'container_nch', ch_num + 1)
             else
@@ -50,7 +51,7 @@ else --  track FX
     local isincontainer, parent_container = r.TrackFX_GetNamedConfigParm(track, fxidx, 'parent_container')  
     if isincontainer then -- for container FX
       local _, hm_ch = r.TrackFX_GetNamedConfigParm(track, parent_container, 'container_nch')
-      if ch_num > hm_ch then
+      if ch_num > tonumber(hm_ch) then
         if ch_num % 2 == 1 then
             r.TrackFX_SetNamedConfigParm(track, parent_container, 'container_nch', ch_num + 1)
         else
@@ -72,3 +73,4 @@ else --  track FX
     r.TrackFX_SetPinMappings(track, fxidx, 1, 0 + 0x1000000, n_low32l, n_high32l) -- #3 output 1, #4 pin 0 left
     r.TrackFX_SetPinMappings(track, fxidx, 1, 1 + 0x1000000, n_low32r, n_high32r) -- #4 pin 1 right
 end
+r.Undo_EndBlock("Paste channel pin mappings into last touched FX's stereo output channel mappings ", -1)
