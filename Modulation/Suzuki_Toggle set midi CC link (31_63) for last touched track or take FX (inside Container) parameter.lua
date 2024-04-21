75e1873e7b44e732ec029aec838e09e4c5cdb74a
Suzuki <101462564+Suzuki-Re@users.noreply.github.com>
2024-04-22 05:14:17 +0900

    v1.1

--- Modulation/Suzuki_Toggle set midi CC link (31_63) for last touched track or take FX (inside Container) parameter.lua
+++ Modulation/Suzuki_Toggle set midi CC link (31_63) for last touched track or take FX (inside Container) parameter.lua
@@ -1,8 +1,8 @@
 -- @description Toggle set midi CC link (31/63) for last touched track or take FX (inside Container) parameter
 -- @author Suzuki
 -- @license GPL v3
--- @version 1.0
--- @changelog Init
+-- @version 1.1
+-- @changelog Added an option value to change bus and channel
 -- @about Using v7.0+ API
 
 local r = reaper
@@ -10,23 +10,35 @@ local r = reaper
 local retval, trackidx, itemidx, takeidx, fxidx, parm = r.GetTouchedOrFocusedFX(0)
 local track = r.CSurf_TrackFromID(trackidx + 1, false) -- 1 based
 
+----------------------- Change the values to whatever you like -----------------------
+
 local WhichCCValue = 31 -- CC value(CC=0_119/14bit=0_31)
 local Is14bit = 1 -- 14bit (yes=1/no=0)
+local Bus = 0 -- 0 based (0 = bus 1, 1 = bus 2 etc)
+local Channel = 1 -- 1 based (1 = chan 1, 2 = chan 2 etc but 0 = omni)
+
+--------------------------------------------------------------------------------------
 
 local function GetUserInputCC()
     if Is14bit ~= nil then
         if type(WhichCCValue) == "string" then
             local input1check = tonumber(WhichCCValue)
             local input2check = tonumber(Is14bit)
-            if input1check and input2check then
+            local input3check = tonumber(Bus)
+            local input4check = tonumber(Channel)
+            if input1check and input2check and input3check and input4check then
                 WhichCCValue = input1check
                 Is14bit = input2check
+                Bus = input3check
+                Channel = input4check
             else
                 error('Only enter a number')
             end 
         end 
         local WhichCCValue = tonumber(WhichCCValue)
-        local Is14bit = tonumber(Is14bit)                                                      
+        local Is14bit = tonumber(Is14bit)   
+        Bus = tonumber(Bus)
+        Channel = tonumber(Channel)                                                      
         if Is14bit < 0 then  
             Is14bit = 0
         elseif Is14bit > 1 then
@@ -39,10 +51,20 @@ local function GetUserInputCC()
         elseif Is14bit == 1 and WhichCCValue > 31 then
             WhichCCValue = 31
         end
+        if Bus < 0 then  
+            Bus = 0
+        elseif Bus > 15 then
+            Bus = 15
+        end
+        if Channel < 0 then  
+            Channel = 0
+        elseif Channel > 16 then
+            Channel = 16
+        end
         Is14bit = Is14bit * 128
         retvals = WhichCCValue + Is14bit
     end
-    return retvals
+    return retvals, Bus, Channel
 end
 
 if itemidx ~= -1 then
@@ -58,15 +80,15 @@ if itemidx ~= -1 then
         r.TakeFX_SetNamedConfigParm(take, fxidx, "param."..parm..".plink.midi_msg", 0)
         r.TakeFX_SetNamedConfigParm(take, fxidx, "param."..parm..".plink.midi_msg2", 0)
     else
-        local retvals = GetUserInputCC()
+        local retvals, Bus, Channel = GetUserInputCC()
         if retvals then
-        r.TakeFX_SetNamedConfigParm(take, fxidx, "param."..parm..".plink.active", 1)
-        r.TakeFX_SetNamedConfigParm(take, fxidx, "param."..parm..".plink.effect", -100)
-        r.TakeFX_SetNamedConfigParm(take, fxidx, "param."..parm..".plink.param", -1)
-        r.TakeFX_SetNamedConfigParm(take, fxidx, "param."..parm..".plink.midi_bus", 0)
-        r.TakeFX_SetNamedConfigParm(take, fxidx, "param."..parm..".plink.midi_chan", 1)
-        r.TakeFX_SetNamedConfigParm(take, fxidx, "param."..parm..".plink.midi_msg", 176)
-        r.TakeFX_SetNamedConfigParm(take, fxidx, "param."..parm..".plink.midi_msg2", retvals)
+            r.TakeFX_SetNamedConfigParm(take, fxidx, "param."..parm..".plink.active", 1)
+            r.TakeFX_SetNamedConfigParm(take, fxidx, "param."..parm..".plink.effect", -100)
+            r.TakeFX_SetNamedConfigParm(take, fxidx, "param."..parm..".plink.param", -1)
+            r.TakeFX_SetNamedConfigParm(take, fxidx, "param."..parm..".plink.midi_bus", Bus)
+            r.TakeFX_SetNamedConfigParm(take, fxidx, "param."..parm..".plink.midi_chan", Channel)
+            r.TakeFX_SetNamedConfigParm(take, fxidx, "param."..parm..".plink.midi_msg", 176)
+            r.TakeFX_SetNamedConfigParm(take, fxidx, "param."..parm..".plink.midi_msg2", retvals)
         end
     end
 else
@@ -80,13 +102,13 @@ else
         r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.midi_msg", 0)
         r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.midi_msg2", 0)
     else
-        local retvals = GetUserInputCC()
+        local retvals, Bus, Channel = GetUserInputCC()
         if retvals then
             r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.active", 1)
             r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.effect", -100)
             r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.param", -1)
-            r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.midi_bus", 0)
-            r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.midi_chan", 1)
+            r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.midi_bus", Bus)
+            r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.midi_chan", Channel)
             r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.midi_msg", 176)
             r.TrackFX_SetNamedConfigParm(track, fxidx, "param."..parm..".plink.midi_msg2", retvals)
         end
