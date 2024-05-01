aeddbd137040ca3436c6f1ef118188bb27a52f2a
Suzuki <101462564+Suzuki-Re@users.noreply.github.com>
2024-05-01 18:22:34 +0900

    v1.2

--- Track/Track Template Shortcut Generator/Suzuki_Track template shortcut generator.lua
+++ Track/Track Template Shortcut Generator/Suzuki_Track template shortcut generator.lua
@@ -1,9 +1,9 @@
 -- @description Suzuki Track Template Shortcut Generator
 -- @author Suzuki
 -- @license GPL v3
--- @version 1.1
+-- @version 1.2
 -- @changelog 
---   # imgui shims
+--   # Updated ReaImGui v0.9.0+
 -- @about 
 --   # Track Template Shortcut Generator
 --   Track Template Shortcut Generator creates scripts to load your track templates. 
@@ -11,21 +11,27 @@
 --   Left clicking the button opens your track template resource folder. Simply drop your ".RTrackTemplate" files to the button from there.
 --   The generated script will be automatically saved to the folder "Suzuki Scripts/Track Template Shortcut Generator/Insert Track Template Scripts/", and will also be registered to the action list.
 --   ### Prerequisites
---   ReaImGui
-
-package.path = reaper.ImGui_GetBuiltinPath() .. '/?.lua'
-require 'imgui' '0.8.7'
-
-local input_title = "Track Template Shortcut Generator"
+--   ReaImGui, js extension
 
 r = reaper
 
+package.path = r.ImGui_GetBuiltinPath() .. '/?.lua'
+im = require 'imgui' '0.9.0.2'
+
+local input_title = "Track Template Shortcut Generator"
+
 local OS = r.GetOS()
+
+if OS == "Win64" or OS == "Win32" then
+  slash = "\\"
+else
+  slash = "/"
+end
 
 local ResourcePath = r.GetResourcePath()
 local folder_path = "/Scripts/Suzuki Scripts/Track/Track Template Shortcut Generator/Insert Track Template Scripts/"
 
-if not r.ImGui_CreateContext then
+if not im.CreateContext then
   r.MB("Missing dependency: ReaImGui extension.\nDownload it via Reapack ReaTeam extension repository.", "Error", 0)
   return false
 end
@@ -45,7 +51,24 @@ local function GenerateFiles(count)
   local files = {}
   local paths = {}
   for i = 1, count do
-    local rv, payload = r.ImGui_GetDragDropPayloadFile(ctx, i - 1) -- 0 based
+    ext = d[i]:match("(%.%w+)$")
+    local filename = d[i]:match("([^\\/]+)%.%w%w*$")
+    r.RecursiveCreateDirectory(ResourcePath .. folder_path, 0) -- create folder if it's not there
+    files[i] = string.format("%s" .. folder_path .. "/Suzuki_Insert " .. filename .. " RTrackTemplate.lua", ResourcePath, i)
+    if OS == "Win64" or OS == "Win32" then
+      payload = d[i]:gsub("\\", "/")
+      ResourcePath = ResourcePath:gsub("\\", "/")
+    end
+    paths[i] = string.match(payload, ResourcePath .. "/(.*)")
+  end
+  return files, paths
+end
+
+local function DNDGenerateFiles(count)
+  local files = {}
+  local paths = {}
+  for i = 1, count do
+    local rv, payload = im.GetDragDropPayloadFile(ctx, i - 1) -- 0 based
     ext = payload:match("(%.%w+)$")
     local filename = payload:match("([^\\/]+)%.%w%w*$")
     r.RecursiveCreateDirectory(ResourcePath .. folder_path, 0) -- create folder if it's not there
@@ -84,10 +107,10 @@ local function Register(files, count)
 end
 
 local function DndAddTT_TARGET()
-  if r.ImGui_BeginDragDropTarget(ctx) then
-    local rv, count = r.ImGui_AcceptDragDropPayloadFiles(ctx)
+  if im.BeginDragDropTarget(ctx) then
+    local rv, count = im.AcceptDragDropPayloadFiles(ctx)
     if rv then
-      files, paths = GenerateFiles(count)
+      files, paths = DNDGenerateFiles(count)
       if ext == ".RTrackTemplate" then
         local code = CreateScripts(paths)
         CreateFiles(files, code)
@@ -96,17 +119,39 @@ local function DndAddTT_TARGET()
         return
       end
     end
-    r.ImGui_EndDragDropTarget(ctx)
+    im.EndDragDropTarget(ctx)
   end
 end
 
 local function Main()
-  local rv = r.ImGui_Button(ctx, "Drop Track Template Files Here", 300, 300)
+  local rv = im.Button(ctx, "Drop Track Template Files Here", 300, 300)
   if rv then
-    if OS == "Win64" or OS == "Win32" then
-      os.execute('start ' .. ResourcePath .. "/TrackTemplates")
+    if r.HasExtState("Track Template Shortcut Generator", "template_files") then
+      dir = r.GetExtState("Track Template Shortcut Generator", "template_files")
     else
-      os.execute('open ' .. ResourcePath .. "/TrackTemplates")
+      dir = ResourcePath .. slash .. "TrackTemplates"
+    end
+    local rv, files = r.JS_Dialog_BrowseForOpenFiles('Select track template files', dir, '', "TrackTemplates\0*.RTrackTemplate\0\0", true)
+    if rv and files:len() > 0 then
+      d = {} -- https://forum.cockos.com/showpost.php?p=2107274&postcount=299
+      for file in files:gmatch("[^\0]*") do -- * instead of + to capture empty substrings
+        d[#d+1] = file
+      end
+      if #d > 1 then -- multiple files?
+        local folder = table.remove(d, 1) -- may be empty, in case of macOS
+        for f = 1, #d do
+          d[f] = folder .. d[f]
+        end
+      end
+      files, paths = GenerateFiles(#d)
+      if ext == ".RTrackTemplate" then
+        local code = CreateScripts(paths)
+        CreateFiles(files, code)
+        Register(files, #d)
+      else
+        return
+      end
+      r.SetExtState("Track Template Shortcut Generator", "template_files", d[#d], true)
     end
   end
   DndAddTT_TARGET()
@@ -115,20 +160,20 @@ end
 local function Run()
 
   if set_dock_id then
-    r.ImGui_SetNextWindowDockID(ctx, set_dock_id)
+    im.SetNextWindowDockID(ctx, set_dock_id)
     set_dock_id = nil
   end
 
-  r.ImGui_SetNextWindowSizeConstraints(ctx, 325, 330, 340, 350)
-  r.ImGui_SetNextWindowSize(ctx, 325, 330, r.ImGui_Cond_FirstUseEver())
-  local imgui_visible, imgui_open = r.ImGui_Begin(ctx, input_title, true, r.ImGui_WindowFlags_NoScrollbar())
+  im.SetNextWindowSizeConstraints(ctx, 325, 330, 340, 350)
+  im.SetNextWindowSize(ctx, 325, 330, im.Cond_FirstUseEver)
+  local imgui_visible, imgui_open = im.Begin(ctx, input_title, true, im.WindowFlags_NoScrollbar)
 
   if imgui_visible then
     Main()
-    r.ImGui_End(ctx)
+    im.End(ctx)
   end
 
-  if process or not imgui_open or r.ImGui_IsKeyPressed(ctx, 27) then -- 27 is escaped key
+  if process or not imgui_open or im.IsKeyPressed(ctx, im.Key_Escape) then -- 27 is escaped key
     imgui_open = nil
   else
     r.defer(Run)
@@ -138,7 +183,7 @@ end
 local function Init()
   SetButtonState(1)
   r.atexit(Exit)
-  ctx = r.ImGui_CreateContext(input_title,  r.ImGui_ConfigFlags_DockingEnable())
+  ctx = im.CreateContext(input_title,  im.ConfigFlags_DockingEnable)
   Run()
 end
 
